4a2c63ed466b3cddf36c8d5176ec628438b3db82
ByCo_ <byco1685@gmail.com>
2024-12-23 12:13:56 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,26 @@
+
+    const smoke_frame = [
+        'imagenes/frame_picante/fuego1.png',
+        'imagenes/frame_picante/fuego2.png', 
+        'imagenes/frame_picante/fuego3.png', 
+        'imagenes/frame_picante/fuego4.png',
+        'imagenes/frame_picante/fuego5.png', 
+        'imagenes/frame_picante/fuego6.png'
+    ];
+    
+    const animation_smoke = document.getElementById('humo');
+    let currentIndex = 0;
+    
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % smoke_frame.length; // Cicla de imágenes
+        animation_smoke.src = smoke_frame[currentIndex]; // Cambia la imagen
+    }
+    
+    // Llama a la función changeImage cada 500 ms (0.5 segundos)
+    if (animation_smoke) {
+        setInterval(changeImage, 100);
+    } else {
+        console.error("El elemento con id 'humo' no fue encontrado en el DOM.");
+    }
+    
+    
